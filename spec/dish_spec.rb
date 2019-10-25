e04c4fa6bbd33d26d312f4cf3b2d8eda453f4b17
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-25 14:01:09 +0100

    edited again

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,9 +1,15 @@
 require 'dish'
 describe Dish do
-    
- it "should show the list of dishes with prices" do
+
+ it "should show the name of the dish" do
     dish = Dish.new("Pasta", "7")
     expect(dish.name).to eq "Pasta"
 end
+
+it "should show the price of a dish" do
+    dish = Dish.new("Pasta", "7")
+    expect(dish.price).to eq "7"
+end 
+
 
 end
