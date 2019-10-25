076abc39938a1798cf20853a3349d8c8cd564d91
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-25 13:31:04 +0100

    edited first test

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,11 +1,3 @@
 class Dish
 
 end
-
-
-
-
-
-
-
-#I would like to see a list of dishes with prices
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,9 +1,6 @@
 require 'dish'
 describe Dish do
-it "to create a new class" do
-    dish = Dish.new
-    expect(dish).to be_instance_of Dish
-end
+    
  it "should show the list of dishes with prices" do
     dish = Dish.new("Pasta", "7")
     expect(dish.name).to eq "Pasta"
