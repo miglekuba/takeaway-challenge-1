158f70994e1bad77d6ece218c2d13e108dbf7258
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-25 15:50:30 +0100

    added menu instance

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,10 @@
 class Takeaway
-  
+    attr_reader :menu
+    def initialize
+        @menu = [
+            {name: "Pasta", price: "7"},
+            {name: "Salad", price: "4"}, 
+            {name: "Soup", price: "2"}
+        ]
+    end
 end
